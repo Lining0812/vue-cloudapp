949c83c3f09fe7a1195e647ba728a9b4cde74ae0
Em0o0 <934615279@qq.com>
2025-09-29 20:32:03 +0800

    update-0929

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
   <RouterView></RouterView>
   <btncomponent/>
   <cardcomponent/>
+  <TestButton/>
 </template>
 
 <script>
 import btncomponent from './components/Test.vue'
 import cardcomponent from './components/TestCard.vue'
+import TestButton from './components/TestButton.vue';
 
 export default{
   components:{
     btncomponent,
-    cardcomponent
+    cardcomponent,
+    TestButton
   }
 }
 
