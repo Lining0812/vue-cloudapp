24791f82b0638924aa08dfdd25415be719ac99cd
Em0o0 <934615279@qq.com>
2025-10-23 18:48:58 +0800

    update-1023

---
+++ src/components/Menu.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="menubox">
+        <div class="menubox_menu">
+            <div class="menubox_menu_content">
+                <p>AAA</p>
+                <button @click="emits('update:isActive', false)">Close Nav</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import gsap from 'gsap';
+import { onMounted, watch } from 'vue';
+
+// 父子组件参数传递
+const props = defineProps({
+    isActive:Boolean
+})
+const emits = defineEmits(['update:isActive'])
+
+// 侦听参数
+watch(()=>props.isActive,(newVal)=>{navAnimation(newVal);});
+
+const tl = gsap.timeline({ paused: true });
+function navAnimation(bool){
+      if(bool){
+        tl.play();
+      }else{
+        tl.reverse();
+      }
+      console.log("参数是:"+bool)
+}
+
+onMounted(()=>{
+    tl.set('body',{overflow:'hidden'})
+      .set('#menubox',{zIndex:1000})
+      .fromTo('.menubox_menu',{yPercent:100},{duration:2,yPercent:0})
+})
+
+</script>
+
+<style>
+#menubox{
+    position: fixed;
+    z-index: -1000;
+}
+
+.menubox_menu{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: antiquewhite;
+}
+
+.menubox_menu_content{
+    position: relative;
+}
+
+</style>
