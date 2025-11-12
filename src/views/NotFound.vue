aaf4902a06992c3f224c1b9aaba9d28603639d78
Em0o0 <934615279@qq.com>
2025-11-12 20:31:16 +0800

    update-1112

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,42 +1,42 @@
 <template>
     <!-- <h1>404 Not Found</h1> -->
-     <div class="intro"></div>
+    <div class="intro"></div>
 
-     <div ref="racesWrapper" class="racesWrapper">
+    <div ref="racesWrapper" class="racesWrapper">
         <div ref="races" class="races">
-            <div><h2>Monaco</h2></div>
-            <div><h2>Austria</h2></div>
-            <div ref="hungray" class="hungray"><h2>Hungary</h2></div>
-            <div><h2>Netherlands</h2></div>
-            <div><h2>China</h2></div>
+            <h2>Monaco</h2>
+            <h2>Austria</h2>
+            <h2>Hungary</h2>
+            <h2>Netherlands</h2>
+            <h2>China</h2>
         </div>
-     </div>
+    </div>
 
-     <div class="outro"></div>
+    <div class="outro"></div>
 </template>
 
 <style scoped>
-.intro{
+.intro {
     width: 100%;
     height: 50vh;
     background-color: gray;
 }
 
-.racesWrapper{
+.racesWrapper {
     overflow: hidden;
 }
 
-.races{
+.races {
     width: fit-content;
     display: flex;
     flex-direction: row;
-    gap: 10rem;
+    /* gap: 10rem; */
     font-size: 80px;
     font-weight: 800;
     color: tomato;
 }
 
-.outro{
+.outro {
     width: 100%;
     height: 100vh;
     background-color: gray;
@@ -44,47 +44,50 @@
 </style>
 
 <script setup>
-import { ref,onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const races = ref(null);
-const hungray = ref(null);
 
-onMounted(()=>{
+onMounted(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    console.log(races);
-    const element = races.value.querySelectorAll('h2');
-    console.log(element);
+    // const elements = races.value.querySelectorAll('h2');
+    const elements = gsap.utils.toArray('h2', races.value);
 
-    const tween = gsap.to(races.value,{
-    x:getScrollAmount(),
-    duration:3,
-    ease:'none',
- });
+    const tween = gsap.fromTo(races.value, {
+        x: () => window.innerWidth,
+    },
+        {
+            x: getScrollAmount(),
+            duration: 3,
+            ease: 'none',
+        });
 
- ScrollTrigger.create({
-    trigger:'.racesWrapper',
-    start:'top 20%',
-    end:()=>`+=${Math.abs(getScrollAmount())}`,
-    pin:true,
-    animation:tween,
-    scrub:1,
-    markers:true
- })
+    ScrollTrigger.create({
+        trigger: '.racesWrapper',
+        start: 'top 20%',
+        end: () => `+=${Math.abs(getScrollAmount())}`,
+        pin: true,
+        animation: tween,
+        scrub: 1,
+        markers: true
+    })
 
- ScrollTrigger.create({
-    trigger:'.hungray',
-    start:'left center',
-    animation:gsap.to('.hungray h2',{scale:0.5, opacity:0.5}),
-    containerAnimation:tween,
-    scrub:true,
-    markers:true
- })
-})
+    elements.forEach(element => {
+        ScrollTrigger.create({
+            trigger: element,
+            start: 'left center',
+            animation: gsap.to(element, { scale: 0.5, opacity: 0.5 }),
+            containerAnimation: tween,
+            scrub: true,
+            markers: true
+        });
+    });
+});
 
-function getScrollAmount(){
-    return -(races.value.offsetWidth - window.innerWidth);
+function getScrollAmount() {
+    return -(races.value.offsetWidth - window.innerWidth / 2);
 }
 </script>
