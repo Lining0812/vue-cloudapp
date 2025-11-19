e40f7f0ca5f88b67dc80347a0724cd97f5d3eebc
Em0o0 <934615279@qq.com>
2025-11-19 18:39:04 +0800

    update-1119

--- src/components/MenuOverlay.vue
+++ src/components/MenuOverlay.vue
@@ -7,19 +7,16 @@
                 </div>
                 <div class="col-sm">
                     <div class="menu-links">
-                        <div class="link" v-for="(route,index) in routes" :key="index">
-                            <router-link 
-                            :to="{name:route.name}" 
-                            data-img="../assets/images/T1.jpg">
-                            {{ route.title }}
-                        </router-link>
+                        <div class="link" v-for="(route, index) in routes" :key="index">
+                            <router-link :to="{ name: route.name }" data-img="../assets/images/T1.jpg">
+                                {{ route.title }}
+                            </router-link>
                         </div>
                     </div>
                     <div class="menu-socials">
-                        <div class="social"><a href="https://weibo.com/n/%E9%BB%84%E9%9C%84%E9%9B%B2?from=feed&loc=at" target="_blank">Home1</a></div>
-                        <div class="social"><a href="https://www.douyin.com/user/MS4wLjABAAAAjW0gMk6HfnozLjpBmla_Ad2igcU4EkqV6WwnkK0ZuNM?from_tab_name=main&is_search=1&list_name=follow&nt=0" target="_blank">Home2</a></div>
-                        <div class="social"><a href="https://www.xiaohongshu.com/user/profile/5e6cc87f0000000001003f24?xsec_token=ABv83ps5rrhzyZOslpS79yphATmpcFcFLPhQtP_GRCaqQ%3D&xsec_source=pc_search" target="_blank">Home3</a></div>
-                        <div class="social"><a href="#">Home4</a></div>
+                        <div class="social" v-for="(social, index) in socials" :key="index">
+                            <a :href="social.url" target="_blank">{{ social.title }}</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,18 +38,26 @@ import gsap from 'gsap';
 import { ref, watch } from 'vue';
 
 const routes = [
-    {name:"home",title:"主页|Home",},
-    {name:"albumlist",title:"专辑|Album",},
-    {name:"concertlist",title:"演唱会|Concert",},
-    {name:"origins",title:"起源|Origins",},
-    {name:"test",title:"测试|test",},
-    {name:"testupload",title:"testupload",},
+    { name: "home", title: "主页|Home", },
+    { name: "albumlist", title: "专辑|Album", },
+    { name: "concertlist", title: "演唱会|Concert", },
+    { name: "origins", title: "起源|Origins", },
+    { name: "test", title: "测试|test", },
+    { name: "testupload", title: "testupload", },
 ]
 
+const socials = [
+    { url: 'https://space.bilibili.com/501005668', title: "哔哩哔哩" },
+    { url: 'https://weibo.com/n/%E9%BB%84%E9%9C%84%E9%9B%B2', title: "微博" },
+    { url: 'https://www.douyin.com/user/MS4wLjABAAAAjW0gMk6HfnozLjpBmla_Ad2igcU4EkqV6WwnkK0ZuNM', title: "抖音" },
+    { url: 'https://www.xiaohongshu.com/user/profile/5e6cc87f0000000001003f24', title: "小红书" },
+]
+
+
 const props = defineProps({
-    isOpen:{
-        type:Boolean,
-        default:false,
+    isOpen: {
+        type: Boolean,
+        default: false,
     }
 });
 
@@ -61,97 +66,66 @@ const menuContent = ref(null);
 
 // 监听属性并执行动画
 watch(() => props.isOpen, (newVal) => {
-    if(newVal) {
+    if (newVal) {
         openMenu();
     } else {
         closeMenu();
     }
 });
 
-// onMounted(() => {
-//     // 初始隐藏链接
-//     gsap.set(['.link a','.social a'],{
-//         y:'120%',
-//         opacity:0
-//     });
-// });
-
-function openMenu(){
-    // gsap.to(container,{
-    //     rotation:10,
-    //     x:300,
-    //     y:450,
-    //     scale:1.5,
-    //     duration:1.25,
-    //     ease:'power4.inOut',
-    // });
-
-    gsap.to(menuContent.value,{
-        rotation:0,
-        x:0,
-        y:0,
-        scale:1,
-        duration:1.25,
-        ease:'power4.inOut',
+function openMenu() {
+    gsap.to(menuContent.value, {
+        rotation: 0,
+        x: 0,
+        y: 0,
+        scale: 1,
+        duration: 1.25,
+        ease: 'power4.inOut',
     });
-    gsap.to(['.link a','.social a'],{
-        y:"0%",
-        opacity:1,
-        duration:1,
-        delay:0.75,
-        stagger:0.1,
-        ease:'power3.out',
+    gsap.to(['.link a', '.social a'], {
+        y: "0%",
+        opacity: 1,
+        duration: 1,
+        delay: 0.75,
+        stagger: 0.1,
+        ease: 'power3.out',
     });
-    gsap.to(menuOverlay.value,{
-        clipPath:'polygon(0% 0%,100% 0%,100% 175%,0% 100%)',
-        duration:1.25,
-        ease:'power4.inOut',
+    gsap.to(menuOverlay.value, {
+        clipPath: 'polygon(0% 0%,100% 0%,100% 175%,0% 100%)',
+        duration: 1.25,
+        ease: 'power4.inOut',
     });
 }
 
-function closeMenu(){
-    // gsap.to(container,{
-    //     rotation:0,
-    //     x:0,
-    //     y:0,
-    //     scale:1,
-    //     duration:1.25,
-    //     ease:'power4.inOut',
-    // });
-    gsap.to(menuContent.value,{
-        rotation:-15,
-        x:-100,
-        y:-100,
-        scale:1.5,
-        // opacity:0.25,
-        duration:1.25,
-        ease:'power4.inOut',
+function closeMenu() {
+    gsap.to(menuContent.value, {
+        rotation: -15,
+        x: -100,
+        y: -100,
+        scale: 1.5,
+        duration: 1.25,
+        ease: 'power4.inOut',
     });
-    gsap.to(menuOverlay.value,{
-        clipPath:'polygon(0% 0%,100% 0%,100% 0%,0% 0%)',
-        duration:1.25,
-        ease:'power4.inOut',
-        onComplete:()=>{
-            gsap.set(['.link a','.social a'],{y:'120%'});
+    gsap.to(menuOverlay.value, {
+        clipPath: 'polygon(0% 0%,100% 0%,100% 0%,0% 0%)',
+        duration: 1.25,
+        ease: 'power4.inOut',
+        onComplete: () => {
+            gsap.set(['.link a', '.social a'], { y: '120%' });
         },
     });
 }
 </script>
 
 <style scoped>
-*{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-}
 
-img{
+img {
     width: 100%;
     height: 100%;
     object-fit: cover;
 }
 
-h1{
+h1 {
     color: #fff;
     font-size: 7rem;
     font-weight: 400;
@@ -159,7 +133,8 @@ h1{
     line-height: 1;
 }
 
-a,p{
+a,
+p {
     position: relative;
     text-decoration: none;
     color: #fff;
@@ -168,7 +143,7 @@ a,p{
     user-select: none;
 }
 
-.menu-overlay{
+.menu-overlay {
     position: fixed;
     width: 100vw;
     height: 100svh;
@@ -176,7 +151,7 @@ a,p{
     z-index: 1;
 }
 
-.menu-content{
+.menu-content {
     position: relative;
     width: 100%;
     height: 100%;
@@ -184,98 +159,101 @@ a,p{
     justify-content: center;
     align-items: center;
     transform-origin: bottom left;
-    will-change: transform,opacity;
+    will-change: transform, opacity;
 }
 
-.menu-items, .menu-footer{
+.menu-items,
+.menu-footer {
     width: 100%;
     padding: 2.5rem;
     display: flex;
     gap: 2.5em;
 }
 
-.col-lg{
+.col-lg {
     flex: 3;
 }
 
-.col-sm{
+.col-sm {
     flex: 2;
 }
 
-.menu-items .col-lg{
+.menu-items .col-lg {
     display: flex;
     justify-content: center;
     align-items: center;
 }
 
-.menu-preview-img{
-    position: relative; 
+.menu-preview-img {
+    position: relative;
     width: 70%;
     height: 100%;
     overflow: hidden;
 }
 
-.menu-preview-img img{
+.menu-preview-img img {
     position: absolute;
-    will-change: transform,opacity;
+    will-change: transform, opacity;
 }
 
-.menu-items .col-sm{
+.menu-items .col-sm {
     padding: 2.5em 0;
     display: flex;
     flex-direction: column;
     gap: 2.5em;
 }
 
-.menu-links{
+.menu-links {
     display: flex;
     flex-direction: column;
     gap: 0.5em;
 }
 
-.menu-socials{
+.menu-socials {
     display: flex;
     flex-direction: row;
     gap: 0.5em;
 }
 
-.link,.social{
+.link,
+.social {
     padding: 6px;
-    clip-path: polygon(0% 0%,100% 0%,100% 100%,0% 100%);
+    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
 }
 
-.link a,.social a{
+.link a,
+.social a {
     display: inline-block;
     will-change: transform;
     transition: color 0.5s;
 }
 
-.link a{
+.link a {
     font-size: 2.5rem;
     letter-spacing: -0.02rem;
 }
 
-.social a{
+.social a {
     color: #8f8f8f;
 }
 
-.social a:hover{
+.social a:hover {
     color: #fff;
 }
 
-.menu-footer{
+.menu-footer {
     position: absolute;
     bottom: 0;
 }
 
-.menu-footer .col-sm{
+.menu-footer .col-sm {
     display: flex;
     justify-content: space-between;
 }
 
 .link a::after,
 .social a::after,
-.menu-footer a::after{
+.menu-footer a::after {
     position: absolute;
     content: '';
     top: 102.5%;
@@ -285,27 +263,28 @@ a,p{
     background: #fff;
     transform: scaleX(0);
     transform-origin: right;
-    transition: transform 0.3s cubic-bezier(0.6,0,0.4,1);
+    transition: transform 0.3s cubic-bezier(0.6, 0, 0.4, 1);
 }
 
 .link a:hover::after,
 .social a:hover::after,
-.menu-footer a:hover::after{
+.menu-footer a:hover::after {
     transform: scaleX(1);
     transform-origin: left;
 }
 
 .link a,
-.social a{
+.social a {
     transform: translateY(120%);
     /* opacity: 0.25; */
 }
-.menu-content{
+
+.menu-content {
     transform: translateX(-100px) translateY(-100px) scale(1.5) rotate(-15deg);
     /* opacity: 0.25; */
 }
-.menu-overlay{
-    clip-path: polygon(0% 0%,100% 0%,100% 0%,0% 0%);
-}
 
+.menu-overlay {
+    clip-path: polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%);
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,63 @@
 <template>
     <div class="intro"></div>
-    <div class="container"></div>
+    <div class="container">
+        <div class="panels">
+            <div class="panel" style="background-color: green">1</div>
+            <div class="panel" style="background-color: blue">2</div>
+            <div class="panel" style="background-color: red">3</div>
+        </div>
+    </div>
     <div class="outro"></div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
+onMounted(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    let panels = gsap.utils.toArray('.panel');
+
+    gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+            trigger: '.container',
+            pin: true,
+            scrub: 1,
+            snap: 1 / (panels.length - 1)
+        }
+    })
+})
 </script>
 
 <style scoped>
-.intro,
-.container,
+.intro {
+    width: 100%;
+    height: 100vh;
+    background-color: palevioletred;
+}
+
+.container {
+    width: 100%;
+    height: 100vh;
+    background-color: tomato;
+    overflow: hidden;
+}
+
+.panels {
+    width: 300%;
+    height: 100%;
+    display: flex;
+}
+
+.panel {
+    width: 100%;
+    height: 100%;
+}
+
 .outro {
     width: 100%;
     height: 100vh;
